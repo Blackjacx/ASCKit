8cf1a4490e2ec2dbfca8d01d3d890684f2768934
Stefan Herold <stefan.herold@gmail.com>
2021-04-18 22:30:39 +0200

    Add constants for button and raster size

--- Sources/ASCKit/Constants.swift
+++ Sources/ASCKit/Constants.swift
@@ -11,3 +11,12 @@ public enum Constants {
     public static let defaultPageSize: UInt = 50
     public static let maxPageSize: UInt = 200
 }
+
+#if canImport(UIKit)
+import UIKit
+
+extension Constants {
+    public static let buttonSize: CGFloat = 44
+    public static let raster: CGFloat = 12
+}
+#endif
