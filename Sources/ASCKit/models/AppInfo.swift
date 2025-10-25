ef1f1b21bf9e96748437b6e1f286a017110073fd
Stefan Herold <794372+Blackjacx@users.noreply.github.com>
2025-10-26 00:26:56 +0200

    feat: boot performance by leveraging JSON includes
    For updating the age ratings this avoids unnecessarily fetching the the
    age rating for each AppInfo object since we can already include it.

--- Sources/ASCKit/models/AppInfo.swift
+++ Sources/ASCKit/models/AppInfo.swift
@@ -17,7 +17,57 @@ public struct AppInfo: IdentifiableModel {
     }
 }
 
+public struct AppInfoResponse: IdentifiableModel {
+    let data: [AppInfo]
+    let included: [IncludedResource]?
+
+    public var id: String = UUID().uuidString
+    public var name: String {
+        id
+    }
+
+    // MARK: - Sub Types
+
+    public enum IncludedResource: Model {
+        case ageRatingDeclaration(_ value: AgeRatingDeclaration)
+
+        public init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            let type = try container.decode(String.self, forKey: .type)
+
+            // Checking of the JSON:API `type` field
+            switch type {
+            case "ageRatingDeclarations":
+                let value = try AgeRatingDeclaration(from: decoder)
+                self = .ageRatingDeclaration(value)
+            default:
+                throw DecodingError.dataCorruptedError(
+                    forKey: .type,
+                    in: container,
+                    debugDescription: "Unknown included type: \(type)"
+                )
+            }
+        }
+
+        public func encode(to encoder: any Encoder) throws {
+            switch self {
+            case .ageRatingDeclaration(let value):
+                try value.encode(to: encoder)
+            }
+        }
+
+        private enum CodingKeys: String, CodingKey {
+            case type
+        }
+
+        public enum Types: String {
+            case ageRatingDeclaration
+        }
+    }
+}
+
 public extension AppInfo {
+
     struct Attributes: Model {
         public var state: AppStoreVersion.State
     }
